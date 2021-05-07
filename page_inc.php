801fd21a422f4e87ef643b69df3f4c6dd7d10adc
PVBK <59559992+pvbk@users.noreply.github.com>
2021-05-07 17:35:39 +0300

    Delete page_inc.php

--- page_inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-//require_once( $_SERVER['DOCUMENT_ROOT'].'/mSite/_config_path/dir.php' );
-//require_once( SITE_CONFIG );
-//require_once( DIR_ADM.'cms_lib'.DSR.'admin_data.php' );
-
-
-
-?>
